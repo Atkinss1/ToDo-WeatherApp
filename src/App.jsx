e0b4055395cdb2bc5752bf504c496c7fe9e5abbf
Atkinss <jordanatkins@live.com>
2024-12-06 21:01:05 -0700

    add: main components, css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import './App.css';
+import { ToDoWrapper } from "./components/ToDoWrapper"
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <ToDoWrapper/>
+    </div>
   )
 }
 
---
+++ src/components/EditToDoForm.jsx
@@ -0,0 +1,5 @@
+export const EditToDoForm = () => {
+  return (
+    <div>EditToDoForm</div>
+  )
+}
