75ad4bcfd53de90e95a53364115ae5bf87cff2e5
Atkinss <jordanatkins@live.com>
2024-12-07 15:48:53 -0700

    add: input form for todos and states

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,5 +1,22 @@
-export const ToDoForm = () => {
+import { useState } from "react"
+import PropTypes from 'prop-types'; 
+
+export const ToDoForm = ({ addTodo }) => {
+
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+  };
   return (
-    <div>ToDoForm</div>
+    <form className='TodoFrom' onSubmit={handleSubmit}>
+      <input onChange={(e) => setValue(e.target.value)} type="text" className="todo-input" placeholder="What is the task to do today?"/>
+      <button type="submit" className="todo-btn">Add Task</button>
+    </form>
   )
 }
+
+ToDoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
--- src/components/ToDoWrapper.jsx
+++ src/components/ToDoWrapper.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+import { ToDoForm } from "./ToDoForm"
+import { v4 as uuidv4 } from 'uuid';
+
+
 export const ToDoWrapper = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: uuidv4(), todo: todo}]);
+  };
+
   return (
-    <div>ToDoWrapper</div>
+    <div className="TodoWrapper">
+      <ToDoForm addTodo={addTodo}/>
+    </div>
   )
 }
