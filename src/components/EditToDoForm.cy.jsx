4e845fbd5a07b0d8de701c12dae41807e04b081d
Atkinss <jordanatkins@live.com>
2024-12-24 22:13:18 -0700

    add: comments to test

--- src/components/EditToDoForm.cy.jsx
+++ src/components/EditToDoForm.cy.jsx
@@ -25,15 +25,34 @@ describe('<EditToDoForm />', () => {
     </TodosContext.Provider>
     );
 
+    // Check if the form is rendered
     cy.get('.TodoForm').should('exist');
-    cy.get('.todo-input').first().should('have.value', 'Test Task');
 
-    cy.get('.todo-input').last().should('exist');
+    // Check if the input fields are rendered
+    cy.get('.todo-input').should('have.length', 2);
+    
+    // Check if the input fields are filled
+    cy.get('.todo-input').first().should('have.value', 'Test Task');
     cy.get('.todo-input').last().should('have.value', 'Test Description');
 
+    // Check if the button is rendered
     cy.get('.todo-btn').should('exist');
+
+    // Check if buttons are rendered
+    cy.get('.todo-btn').should('have.length', 2);
+
+    // Check if the button text is correct
     cy.get('.todo-btn').first().should('have.text', 'Update Task');
+
+    // Check if the button text is correct
     cy.get('.todo-btn').last().should('have.text', 'Cancel');
+
+    // Check if the button is clicked and the task is updated
+    cy.get('.todo-btn').first().click().then(() => {
+      expect(contextValue.editTask).to.have.been.called;
+    });
+
+    // Check if the button is clicked and the task update is cancelled
     cy.get('.todo-btn').last().click().then(() => {
       expect(contextValue.editTodo).to.have.been.called;
       expect(task.isEditing).to.be.false;
