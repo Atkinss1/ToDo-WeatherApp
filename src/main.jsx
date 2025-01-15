e3bd75ce0f6ac90dda9a29c21fc13d86b8d98187
Atkinss <jordanatkins@live.com>
2025-01-15 00:40:55 -0700

    add: react-router for future login feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App.jsx'
 import { TodosContextProvider } from './state/TodosContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <TodosContextProvider>
-      <App />
-    </TodosContextProvider>
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+      <Routes>
+        <Route path='*' element={
+          <TodosContextProvider>
+            <App/>
+          </TodosContextProvider>
+        } />
+      </Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
