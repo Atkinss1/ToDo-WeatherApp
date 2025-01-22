9ef883e27f155122c0cc530308fa190e394a37f1
Atkinss <jordanatkins@live.com>
2025-01-22 08:32:02 -0700

    add: container for weather application, css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
-import { ToDoWrapper } from "./components/ToDoWrapper"
+import { ToDoWrapper } from './components/ToDoWrapper';
 
 function App() {
- 
+	return (
+		<div className='App-Wrapper'>
+			<div className='Weather-App-Wrapper'>
+				<div className='Weather-App'>
+					<p>weather app</p>
+				</div>
+			</div>
 
-  return (
-    <div className='App'>
-      <ToDoWrapper/>
-    </div>
-  )
+			<div className='Todo-App-Wrapper'>
+				<div className='Todo-App'>
+					<ToDoWrapper />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
